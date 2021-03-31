9901900f8dc6dd7bdc4c50b33fcd5d9ab2ae76ab
chegrofelix <chegerofelix@gmail.com>
2021-03-31 21:18:35 +0300

    created database

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'instagram',
+        'USER':'moringa',
+        'PASSWORD':'ragnarok'
     }
 }
 
